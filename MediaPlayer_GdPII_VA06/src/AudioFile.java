34382e04749904aa61a7bf5807b961fe092468c7
kah8361 <kah8361@thi.de>
2017-04-08 21:01:21 +0200

    Testat1

--- MediaPlayer_GdPII_VA06/src/AudioFile.java
+++ MediaPlayer_GdPII_VA06/src/AudioFile.java
@@ -1,34 +1,113 @@
+import java.util.regex.*; 
+import java.io.*;
 
 public class AudioFile {
-    
-    private String pathname;
-    private String filename;
-    
-    
-    public AudioFile(){
-        
-    }
-    
-    //Method checks pathname and saves important stuff in class attributes
-    public void parsePathname(String path){
-       
-        
-        pathname = path;
-        filename = path.substring((path.lastIndexOf('/') + 1), path.length());
-        
-        
-    }
-    
-    //returns path in normal form
-    public String getPathname(){
-        return "";
-    }
-    
-    //return filename without path, or empty string
-    public String getFilename(){
-        return "";
-    }
-    
-    
 
+	private String pathname;
+	private String filename;
+	private static String title;
+	private static String author;
+
+
+	public AudioFile(){
+
+	}
+
+	//Method checks pathname and saves important stuff in class attributes
+	public void parsePathname(String path){
+
+
+		//more than one '/': something with regex following ...	
+
+		regexMultiSlashes(path);
+
+		//don't forget backslashes \\\
+
+		regexBackslashes(path);
+
+
+		//and hard-drive letters D:, C: ... 
+
+		if(path.charAt(1) == ':'){
+
+			path.replace(path.charAt(1), '/');
+			String slash = "/";
+			path = slash + path;
+			
+			
+			int i;
+			for(i=0; i<path.length(); i++){
+
+				if(path.charAt(i) == '/'){
+
+					pathname = path;
+					filename = path.substring((path.lastIndexOf('/') + 1), path.length());
+				}else if(path.charAt(i) != '/'){
+					pathname = path;
+					filename = path;
+				}
+
+
+			}
+
+		}
+	}
+
+	//returns path in normal form
+	public String getPathname(){
+		return pathname;
+	}
+
+	//return filename without path, or empty string
+	public String getFilename(){
+		return filename;
+	}
+
+
+	public static void regexMultiSlashes(String origPathname){
+
+		Pattern replace = Pattern.compile("\\/+");  //Here we put the regex pattern for one or more slashes / // ///
+		Matcher regexMatcher = replace.matcher(origPathname);
+
+		String regexSlashPathname = regexMatcher.replaceAll("/");       
+
+	}
+
+	public static void regexBackslashes(String origPathname){
+
+		Pattern replace = Pattern.compile("\\+");  //Here we put the regex pattern for backslashes \\\
+		Matcher regexMatcher = replace.matcher(origPathname);
+
+		String regexBackslashPathname = regexMatcher.replaceAll("/");       
+
+	}
+
+	public static void parseFilename(String filename){
+
+		if(filename == "-"){
+			author = "";
+			title = filename;
+		}
+		
+		
+		//deletes all whitespaces after and infront of the text
+		filename.trim();
+		
+		
+	}
+
+	public static String getAuthor(){
+		return "Hallo von getAuthor";
+	}
+
+	public static String getTitle(){
+		return "Hallo von getTitle";
+	}
+	
 }
+
+
+
+
+
+	
