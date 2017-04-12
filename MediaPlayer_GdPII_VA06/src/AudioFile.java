3981394d98ad8b8952b6a82cdcbd1edee4e4dd2b
kah8361 <kah8361@thi.de>
2017-04-12 21:20:04 +0200

    Testat1

--- MediaPlayer_GdPII_VA06/src/AudioFile.java
+++ MediaPlayer_GdPII_VA06/src/AudioFile.java
@@ -1,5 +1,7 @@
 import java.util.regex.*; 
 import java.io.*;
+import java.lang.String;
+
 
 public class AudioFile {
 
@@ -38,18 +40,22 @@ public class AudioFile {
 			int i;
 			for(i=0; i<path.length(); i++){
 
+
 				if(path.charAt(i) == '/'){
 
 					pathname = path;
+					
+					if(path.charAt(path.length())== '/'){
+					filename = "";	
+					}else{
 					filename = path.substring((path.lastIndexOf('/') + 1), path.length());
+					}
+					
 				}else if(path.charAt(i) != '/'){
 					pathname = path;
 					filename = path;
 				}
-
-
 			}
-
 		}
 	}
 
@@ -87,24 +93,69 @@ public class AudioFile {
 		if(filename == "-"){
 			author = "";
 			title = filename;
+		}else if(regexNAandNT(filename)){
+			author = "";
+			title = "";
+		}
+		else if(regexMultipleDashes(filename)){
+			//the match = author; rest = title
+		}
+		else if(regexNA(filename)){
+			author = "";
+			title = filename.substring(0, filename.lastIndexOf('.')-1);
 		}
 		
-		
+		else{
 		//deletes all whitespaces after and infront of the text
 		filename.trim();
-		
-		
+		int i;
+		for(i=1; i<filename.length(); i++){
+			if(filename.charAt(i) == '-'){
+				author = filename.substring(0, i-1);
+				author.trim();
+				title = filename.substring(i+1, filename.lastIndexOf('.')-1);   //Example: Author - Title.mp3
+				title.trim();
+			}
+			
+		}
+		if(title.endsWith(".")){
+			title = title.substring(0, title.length()-1);
+		}
+		}
 	}
 
 	public static String getAuthor(){
-		return "Hallo von getAuthor";
+		return author;
 	}
 
 	public static String getTitle(){
-		return "Hallo von getTitle";
+		return title;
+	}
+	
+	
+	public static void regexNA(String origFilename){
+
+		Pattern replace = Pattern.compile("\\[A-Za-z0-9]\\.+\\[a-z0-9]{2,4}");  //Here we put the regex pattern for audiofile.mp3
+		Matcher regexMatcher = replace.matcher(origFilename);      
+
+	}
+	
+	public static void regexNAandNT(String origFilename){
+
+		Pattern replace = Pattern.compile("\\s+\\-+\\s");  //Here we put the regex pattern for >>    -     <<
+		Matcher regexMatcher = replace.matcher(origFilename);      
+
+	}
+	
+	public static void regexMultipleDashes(String origFilename){
+
+		Pattern replace = Pattern.compile("\\[A-Za-z0-9]+\\[-]+\\[A-Za-z0-9]+\\s\\-\\s");  //Here we put the regex pattern for Linkin-Park - Don't stay
+		Matcher regexMatcher = replace.matcher(origFilename);      
+
 	}
 	
 }
+
 
 
 
