8f5df3ee804b7cd8f56052b8e79855d5b21b66c9
kah8361 <kah8361@thi.de>
2017-05-21 21:08:40 +0200

    GUITestat

--- MediaPlayer_GdPII_VA10/src/studiplayer/ui/Player.java
+++ MediaPlayer_GdPII_VA10/src/studiplayer/ui/Player.java
@@ -1,11 +1,26 @@
 package studiplayer.ui;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
 
-@SuppressWarnings("serial")
-public class Player extends JFrame {
+import studiplayer.audio.PlayList;
 
-	public Player(){
+@SuppressWarnings("serial")
+public class Player extends JFrame implements ActionListener{
+	
+	public String songDescriptionS;
+	public String playTimeS;
+	public String windowTitle;
+	
+	public static String DEFAULT_PLAYLIST = "playlists/DefaultPlayList.m3u";
+	
+	public Player(PlayList playlist){
 		//Initialize the main frame
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		try{
@@ -15,13 +30,53 @@ public class Player extends JFrame {
 		}
 		
 		//Create GUI components
+		JLabel songDescription = new JLabel(songDescriptionS);
+		JLabel playTime = new JLabel(playTimeS);
+		JPanel panel = new JPanel();
+		JButton bplay = new JButton(new ImageIcon("icons/play.png"));
+		JButton bpause = new JButton(new ImageIcon("icons/pause.png"));
+		JButton bstop = new JButton(new ImageIcon("icons/stop.png"));
+		JButton bnextTitel = new JButton(new ImageIcon("icons/next.png"));
+		JButton bplaylistEditor = new JButton(new ImageIcon("icons/pl_editor.png"));
+		
+		panel.add(bplay);
+		panel.add(bpause);
+		panel.add(bnextTitel);
+		panel.add(bstop);
+		panel.add(bplaylistEditor);
+		
+		add(songDescription, BorderLayout.NORTH);
+		add(playTime, BorderLayout.WEST);
+		add(panel, BorderLayout.CENTER);
+		
+		setTitle(windowTitle);
+		
 		//Acitvate GUI
 		this.pack();
 		this.setVisible(true);
+		
+		if(playlist.isEmpty()){
+			windowTitle = "Studiplayer: empty playlist";
+			songDescriptionS = "no current Title";
+			playTimeS = "--:--";
+		}else{
+			windowTitle = "Current song: " + playlist.getCurrentAudioFile().toString();
+			songDescriptionS = playlist.getCurrentAudioFile().toString();
+			playTimeS = "00:00";
+		}
 	}
 	
 	public static void main(String[] args){
-		new Player();
+		
+		PlayList playlist = new PlayList();
+		
+		if(args.length == 2){
+			playlist.loadFromM3U(args[1]);
+		}else{
+			playlist.loadFromM3U(DEFAULT_PLAYLIST);
+		}
+		
+		new Player(playlist);
 	}
 	
 }
